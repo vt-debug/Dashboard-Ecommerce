fcf8d0ed55b008df5d972b7ef6d0b73c2691b1a8
vt-debug <pimpolhoplays8@gmail.com>
2025-10-29 18:57:41 -0300

    conclusão paginas

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
